442851eece048ff05ed720895ada1e73203bc4d5
michellobbos <michellobbos@gmail.com>
2023-12-06 10:08:08 +0100

    aktion-aussuchenB

--- src/main/kotlin/Barbaren.kt
+++ src/main/kotlin/Barbaren.kt
@@ -2,7 +2,7 @@ open class Barbaren(name: String): Held(name, 750) {
 
 
     fun angriffBarbar(ziel: Gegner) {
-        angriff(ziel,  SCHADENSWERT + 10)
+        angriff(ziel, SCHADENSWERT + 10)
 
 
     }
@@ -12,10 +12,10 @@ open class Barbaren(name: String): Held(name, 750) {
 
     }
 
-    override fun verteidigungsSchild() {
-        verteidigungsSchild()
-
-    }
+//    override fun verteidigungsSchild() {
+//        verteidigungsSchild()
+//
+//    }
 
     fun bonusAngriffBarbar(ziel: Gegner) {
         bonusAngriff(ziel, BONUSSCHADENSWERT + 50)
@@ -23,31 +23,54 @@ open class Barbaren(name: String): Held(name, 750) {
     }
 
 
+    fun aktionAussuchenBarbar() {
 
-     fun aktionAussuchenBarbar () {
-         println(MENÜ)
-         var auswahl = readln()
-         when (auswahl) {
-             "1" -> {
-                 angriffBarbar(GEGNER)
-             }
+            var menü = """
+             |1 - Angriff
+             |2 - Heilung
+             |3 - VerteidigungSchild
+             |4 - Bonusangriff
+         """.trimMargin()
+            println(menü)
+            repeat(4) {
 
-             "2" -> {
-                 heilungBarbar()
-             }
+            var auswahl = readln()
+            when (auswahl) {
+                "1" -> {
+                    angriffBarbar(GEGNER)
+                    menü = menü.replace("1 - Angriff", "X - Angriff")
+                    println(menü)
 
-             "3" -> {
-                 verteidigungsSchild()
-             }
+                }
 
-             "4" -> {
-                 bonusAngriffBarbar(GEGNER)
-             }
-         }
-     }
+                "2" -> {
+                    heilungBarbar()
+                    menü = menü.replace("2 - Heilung", "X - Heilung")
+                    println(menü)
+
+                }
+
+                "3" -> {
+                    verteidigungsSchild()
+                    menü = menü.replace("3 - VerteidigungSchild", "X - VerteidigungSchild")
+                    println(menü)
+
+                }
+
+                "4" -> {
+                    bonusAngriffBarbar(GEGNER)
+                    menü = menü.replace("4 - Bonusangriff", "X - Bonusangriff")
+                    println(menü)
+
+
+                }
+            }
+        }
+        println("Barbar hat seine vier Aktionen benutzt")
 
 
 
+    }
 
 
 }
